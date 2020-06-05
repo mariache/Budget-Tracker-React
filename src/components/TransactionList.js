1eb46a04622c4459d813072cf447bcdfa57a7a31
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-06 01:40:24 +1200

    - Transaction component;

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import Transaction from "./Transaction";
+import { GlobalContext } from "../context/GlobalState";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.length > 0
+          ? transactions.map((transaction) => (
+              <Transaction key={transaction.id} transaction={transaction} />
+            ))
+          : "No transactions yet"}
       </ul>
     </>
   );
