9216604aa088359e4b4c228c49a5eee8aef192f3
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-06 00:55:06 +1200

    - basic structure;

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
 import "./App.css";
 
 function App() {
-  return <div>Budget tracker</div>;
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
+    </>
+  );
 }
 
 export default App;
